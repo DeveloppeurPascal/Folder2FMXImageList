af52fbfcc51be866b80d74cee7b3e8a0b80aa750
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-08-06 21:08:16 +0200

    released version 1.2 - 20240806

---
+++ src/uConsts.pas
@@ -0,0 +1,47 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Folder to FMX ImageList
+///
+/// Copyright 2022-2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Create a FireMonkey TImageList by importing all images in a folder.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://folder2fmximagelist.olfsoftware.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Folder2FMXImageList
+///
+/// ***************************************************************************
+/// File last update : 2024-08-06T20:00:54.000+02:00
+/// Signature : 7543359d027a64c68453eae2a8c3ba8a7996573b
+/// ***************************************************************************
+/// </summary>
+
+unit uConsts;
+
+interface
+
+const
+  CVersionNumber = '1.2';
+  CVersionDate = '20240806';
+
+implementation
+
+end.
